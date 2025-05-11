a132e62db10ee5c790bbc204d8257d70b2f6e6e7
anastasija Ilievska <aneilievska2002@gmail.com>
2025-05-11 13:52:50 +0200

    Use .env for Firebase config and update .gitignore

--- react-toDos-app-with-firebase-main/src/Firebase/Firebase.config.js
+++ react-toDos-app-with-firebase-main/src/Firebase/Firebase.config.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCWbMDn8pxTNa47Av2IOIaIO9NHcVCe33A",
-  authDomain: "to-dolist-bcca1.firebaseapp.com",
-  projectId: "to-dolist-bcca1",
-  storageBucket: "to-dolist-bcca1.firebasestorage.app",
-  messagingSenderId: "27812424793",
-  appId: "1:27812424793:web:16bdd399a651adf78b5548",
-  measurementId: "G-16489ENWSM"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
